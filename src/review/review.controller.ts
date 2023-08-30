6ce4e6a5893a7ac64f938c4d71d931bd1631d9c4
fabiohidekihirose <fabiohidekihirose@gmail.com>
2023-08-30 22:16:27 +0900

    Feat: controller and model for getting reviews by user and product

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import * as reviewModel from "./review.model";
 
-export async function create(req: Request, res: Response) {
+export async function save(req: Request, res: Response) {
   try {
     const newReview = req.body;
-    await reviewModel.postReview(newReview);
+    await reviewModel.create(newReview);
+    res.status(200).send({});
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function indexUser(req: Request, res: Response) {
+  try {
+    const user_id = parseInt(req.params.userId);
+
+    const reviewsByUser = await reviewModel.getAllByUser(user_id);
+    res.status(200).send(reviewsByUser);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function indexProduct(req: Request, res: Response) {
+  try {
+    const product_id = parseInt(req.params.productId);
+
+    const reviewsByProduct = await reviewModel.getAllByProduct(product_id);
+    res.status(200).send(reviewsByProduct);
   } catch (error: any) {
     res.status(500).send(error.message);
   }
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -3,10 +3,28 @@ import { db } from "../utils/db.server";
 interface Review {
   user_id: number;
   product_id: number;
+  comment: string;
+  rating: number;
 }
 
-export async function postReview(payload: Review) {
+export async function create(payload: Review) {
   return db.review.create({
     data: payload,
+  });
+}
+
+export async function getAllByUser(user_id: number) {
+  return db.review.findMany({
+    where: {
+      user_id,
+    },
+  });
+}
+
+export async function getAllByProduct(product_id: number) {
+  return db.review.findMany({
+    where: {
+      product_id,
+    },
   });
 }
